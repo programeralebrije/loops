45135f7298513fdeb0625516fe30a1cb688a1977
Ruben <ruben.larrazolo@gmail.com>
2021-08-30 19:55:15 -0700

    Adding an Item to the End of the List

--- main.py
+++ main.py
@@ -99,3 +99,58 @@ print(colors_list)
 
 colors_list[-1] = 'purple'
 print(colors_list)
+
+
+# Adding an Item to the End of the List
+
+my_string = "Hello there Bob"
+
+# splits string into list using space as the delimiter/separator
+my_string_items = my_string.split()
+print(my_string_items)
+
+# but we forgot Sally!
+# add 'and' and 'Sally' to the list
+my_string_items.append("and")
+my_string_items.append("Sally")
+
+print(my_string_items)
+
+
+my_list = [1, 2, 3]
+
+# Oops! Forgot 3!
+my_list.insert(3, 4)
+print(my_list)
+
+colors = ['red', 'white', 'blue']
+colors.insert(1, 'green')
+colors.insert(3, 'yellow')
+print(colors)
+
+numbers = [1, 2, 3, 4]
+
+# delete item at index 2
+del numbers[2]
+print(numbers)
+
+# the position of value 4 has changed
+# its index went from 3 to 2, due to the deletion
+
+# delete the first item
+del numbers[0]
+print(numbers)
+
+# delete the last item
+del numbers[-1]
+print(numbers)
+
+
+colors = ['green', 'white', 'green', 'yellw', 'yellw', 'white']
+colors[4] = "yellow"
+colors[3] = "yellow"
+colors.remove("white")
+colors.remove("white")
+colors.append('purple')
+
+print(colors)
