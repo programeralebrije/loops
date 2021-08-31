c302bb7198f75af47718c18eb0b2ffae8fdc041c
Ruben <ruben.larrazolo@gmail.com>
2021-08-30 19:28:34 -0700

    tuple

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 planets = ['Earth', 'Mars', 'Saturn', 'Venus']
 print(planets)
+
+our_world = ['Earth', ['United States', 'Canada', 'Mexico'], [1, 2, 3]]
+print(our_world)
+
+my_tuple = ('doctor', 'nurse', 'PA')
+print(my_tuple)
+
+
+# Mixed Data Type Tuples 
+
+# you've already seen a string tuple
+string_tuple = ("this", "contains", "string", "values")
+
+# you can, of course, create tuples with numbers
+number_tuple = (23, 23.5, 9, 144)
+
+# like lists, you can create tuples with a mix of types
+mixed_tuple = ("Bob", 33, "Sally", 29, "Spike", 8)
+
+
+print(string_tuple , number_tuple, mixed_tuple)
+
+items_witget_tuple = ('white', 'black', 'brown', '25')
+
+print(items_witget_tuple)
+
+items = ('widget', ['white', 'black', 'brown'], 25)
+
+print(items)
+
+colors_list = ['red', 'green', 'blue', 'yellow']
+num_colors = len(colors_list)
+print(num_colors)
