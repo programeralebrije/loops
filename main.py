6aa6783edb98b7a51f97c3c84fb9580ad3c8abdd
Ruben <ruben.larrazolo@gmail.com>
2021-08-30 19:32:29 -0700

    Lists and Tuples Indexing

--- main.py
+++ main.py
@@ -33,3 +33,46 @@ print(items)
 colors_list = ['red', 'green', 'blue', 'yellow']
 num_colors = len(colors_list)
 print(num_colors)
+
+# print the third item of the list
+colors_list = ['red', 'green', 'blue', 'yellow']
+print(colors_list[2])
+
+# print the second item of the tuple
+numbers_tuple = (22, 33, 44, 55)
+print(numbers_tuple[1])
+
+# print the last item of the list
+colors_list = ['red', 'green', 'blue', 'yellow']
+print(colors_list[-1])
+
+# print the last item of the tuple
+numbers_tuple = (22, 33, 44, 55)
+print(numbers_tuple[-1])
+
+colors_list = ['red', 'green', 'blue', 'yellow']
+
+# access the last item with index `-1`
+first_from_end = colors_list[-1]
+
+# access the next-to-last item with index `-2`
+second_from_end = colors_list[-2]
+
+# access the second from the last item with index `-3`
+third_from_end = colors_list[-3]
+
+print(first_from_end)
+print(second_from_end)
+print(third_from_end)
+
+
+colors_list = ['red', 'green', 'blue', 'yellow']
+# Accessing Multiple Items Within a Range
+# get the first three items
+first_three_items = colors_list[0:3]
+
+# get the middle two items
+middle_two_items = colors_list[1:3]
+
+print(first_three_items)
+print(middle_two_items)
